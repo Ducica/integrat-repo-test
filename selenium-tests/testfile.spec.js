4739c91324467cc1d2545934ffbe37568a4231d4
Dusanst <stojanovic@cesnet.cz>
2023-10-03 21:01:47 +0200

    test repo

---
+++ selenium-tests/testfile.spec.js
@@ -0,0 +1,49 @@
+const { By, Builder, Browser } = require("selenium-webdriver");
+const { suite } = require("selenium-webdriver/testing");
+const assert = require("assert");
+const Chrome = require("selenium-webdriver/chrome");
+
+const options = new Chrome.Options();
+
+suite(
+  function (env) {
+    describe("First script", function () {
+      let driver;
+
+      before(async function () {
+        let driver = await new Builder()
+          .setChromeOptions(
+            options.addArguments([
+              "--ignore-ssl-errors=yes",
+              "--ignore-certificate-errors",
+              "--headless",
+            ])
+          )
+          .forBrowser("chrome")
+          .build();
+
+        await driver.manage().setTimeouts({ implicit: 10000 });
+      });
+
+      after(async () => await driver.quit());
+
+      it("First Selenium script", async function () {
+        await driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        let title = await driver.getTitle();
+        assert.equal("Web form", title);
+
+        let textBox = await driver.findElement(By.name("my-text"));
+        let submitButton = await driver.findElement(By.css("button"));
+
+        await textBox.sendKeys("Selenium");
+        await submitButton.click();
+
+        let message = await driver.findElement(By.id("message"));
+        let value = await message.getText();
+        assert.equal("Received!", value);
+      });
+    });
+  },
+  { browsers: [Browser.CHROME, Browser.FIREFOX] }
+);
